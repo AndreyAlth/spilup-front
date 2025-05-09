2bd508ef77163eb369487d023d5a2af0015b791f
gabriel <andreyalth@gmail.com>
2025-05-09 11:20:40 -0600

    resolver rutas relativas

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import jsconfigPaths from 'vite-jsconfig-paths'
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr'
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path'
 
 
 // https://vitejs.dev/config/
@@ -11,4 +12,12 @@ export default defineConfig({
   plugins: [react(), jsconfigPaths(), svgr(),
   eslint(), tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      'i18n': path.resolve(__dirname, './src/i18n'),
+      'styles': path.resolve(__dirname, './src/styles'),
+      'components': path.resolve(__dirname, './src/components'),
+      'utils': path.resolve(__dirname, './src/utils'),
+    }
+  }
 })
