4c0ee62e4e45ff80ca76d6ccb6223fa5aec94ce2
gabriel <andreyalth@gmail.com>
2025-05-16 13:42:23 -0600

    add query provider for react query

---
+++ src/app/contexts/api/QueryProvider.tsx
@@ -0,0 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+export const QueryProvider = ({ children }) => {
+    const client = new QueryClient()
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  )
+}
