ec69e15093e7eaf0b832cdfb1e303de8b756fb4c
gabriel <andreyalth@gmail.com>
2025-05-19 13:05:05 -0600

    add updateuser api

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, UseMutationResult  } from '@tanstack/react-query'
 import axios from 'axios';
 
 export async function useGetUser(userId: string) {
@@ -9,5 +9,19 @@ export async function useGetUser(userId: string) {
             return response;
         }
     })
-
 }
+
+interface UpdateUserResponse {
+    success: boolean;
+    message: string;
+    data?: any; // Adjust this type based on your API response
+  }
+  
+  export const useUpdateUser = (userId: string ): UseMutationResult<UpdateUserResponse, Error, UpdateUser> => {
+    return useMutation<UpdateUserResponse, Error, UpdateUser>({
+      mutationFn: async (userData: UpdateUser) => {
+        const response = await axios.put(`/users/${userId}`, userData);
+        return response.data;
+      }
+    });
+  };
