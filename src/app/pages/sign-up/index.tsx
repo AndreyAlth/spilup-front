ebbdf9a71c0d3804e4d1687b93eab83a5db536f2
gabriel <andreyalth@gmail.com>
2025-05-20 13:47:53 -0600

    add validation to sign up form

--- src/app/pages/sign-up/index.tsx
+++ src/app/pages/sign-up/index.tsx
@@ -1,77 +1,111 @@
 // Import Dependencies
-import { EnvelopeIcon, LockClosedIcon, UserIcon } from "@heroicons/react/24/outline";
+import {
+  EnvelopeIcon,
+  LockClosedIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 import { Link } from "react-router";
 
 // Local Imports
 import { Button, Card, Checkbox, Input } from "components/ui";
 import Logo from "assets/appLogo.svg?react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { createUserSchema } from './schema'
 
 // ----------------------------------------------------------------------
 
 export default function SignUpV1() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(createUserSchema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    console.log('dssdm')
+  };
   return (
     <main className="min-h-100vh grid w-full grow grid-cols-1 place-items-center">
       <div className="w-full max-w-[26rem] p-4 sm:px-5">
         <div className="text-center">
           <Logo className="mx-auto size-16" />
           <div className="mt-4">
-            <h2 className="text-2xl font-semibold text-gray-600 dark:text-dark-100">
+            <h2 className="dark:text-dark-100 text-2xl font-semibold text-gray-600">
               Welcome To Tailux
             </h2>
-            <p className="text-gray-400 dark:text-dark-300">
+            <p className="dark:text-dark-300 text-gray-400">
               Please sign up to continue
             </p>
           </div>
         </div>
         <Card className="mt-5 rounded-lg p-5 lg:p-7">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
             <div className="space-y-4">
               <Input
-                placeholder="Name"
+                label="First Name"
+                placeholder="Enter Name"
                 prefix={
                   <UserIcon
                     className="size-5 transition-colors duration-200"
                     strokeWidth="1"
                   />
                 }
+                {...register("firstName")}
+                error={errors.firstName?.message}
               />
               <Input
-                placeholder="Last Name"
+                label="Last Name"
+                placeholder="Enter Last Name"
                 prefix={
                   <UserIcon
-                    className="size-5 transition-colors duration-200"
-                    strokeWidth="1"
+                  className="size-5 transition-colors duration-200"
+                  strokeWidth="1"
                   />
                 }
+                {...register("lastName")}
+                error={errors.lastName?.message}
               />
               <Input
-                placeholder="Email"
+                label="Email"
+                placeholder="Enter Email"
                 prefix={
                   <EnvelopeIcon
-                    className="size-5 transition-colors duration-200"
-                    strokeWidth="1"
+                  className="size-5 transition-colors duration-200"
+                  strokeWidth="1"
                   />
                 }
+                {...register("email")}
+                error={errors.email?.message}
               />
               <Input
-                placeholder="Password"
+                label="Password"
+                placeholder="Enter Password"
                 type="password"
                 prefix={
                   <LockClosedIcon
-                    className="size-5 transition-colors duration-200"
-                    strokeWidth="1"
+                  className="size-5 transition-colors duration-200"
+                  strokeWidth="1"
                   />
                 }
+                {...register("password")}
+                error={errors.password?.message}
               />
               <Input
+                label="Repeat Password"
                 placeholder="Repeat Password"
                 type="password"
                 prefix={
                   <LockClosedIcon
-                    className="size-5 transition-colors duration-200"
-                    strokeWidth="1"
+                  className="size-5 transition-colors duration-200"
+                  strokeWidth="1"
                   />
                 }
+                {...register("confirmPassword")}
+                error={errors.confirmPassword?.message}
               />
 
               {/* <div className="flex gap-1">
@@ -85,15 +119,15 @@ export default function SignUpV1() {
               </div> */}
             </div>
 
-            <Button className="mt-5 w-full" color="primary">
+            <Button className="mt-5 w-full" color="primary" type="submit">
               Sign Up
             </Button>
           </form>
-          <div className="mt-4 text-center text-xs-plus">
+          <div className="text-xs-plus mt-4 text-center">
             <p className="line-clamp-1">
               <span>Already have an account? </span>{" "}
               <Link
-                className="text-primary-600 transition-colors hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-600"
+                className="text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-600 transition-colors"
                 to="/login"
               >
                 Sign In
@@ -124,9 +158,9 @@ export default function SignUpV1() {
             </Button>
           </div> */}
         </Card>
-        <div className="mt-8 flex justify-center text-xs text-gray-400 dark:text-dark-300">
+        <div className="dark:text-dark-300 mt-8 flex justify-center text-xs text-gray-400">
           <a href="##">Privacy Notice</a>
-          <div className="mx-2.5 my-0.5 w-px bg-gray-200 dark:bg-dark-500"></div>
+          <div className="dark:bg-dark-500 mx-2.5 my-0.5 w-px bg-gray-200"></div>
           <a href="##">Term of service</a>
         </div>
       </div>
---
+++ src/app/pages/sign-up/schema.ts
@@ -0,0 +1,13 @@
+import * as Yup from 'yup'
+
+export const createUserSchema = Yup.object().shape({
+    firstName: Yup.string().trim().required('First name is required'),
+    lastName: Yup.string().trim().required('Last name is required'),
+    email: Yup.string().trim().email('Invalid email format').required('Email is required'),
+    password: Yup.string().trim()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Password is required'),
+    confirmPassword: Yup.string().trim()
+        .oneOf([Yup.ref('password'), null], 'Passwords must match')
+        .required('Confirm password is required'),
+})
